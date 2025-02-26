34e25098a08604eb402f86b46a43d67a790aa0b3
Rachit <trachit752@gmail.com>
2025-02-26 10:30:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import nodemailer from "nodemailer";
+import cors from 'cors'
+
+const prisma = new PrismaClient();
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS,
+  },
+});
+
+app.post("/api/referrals", async (req, res) => {
+  try {
+    const { name, email, referralCode } = req.body;
+
+    if (!name || !email || !referralCode) {
+      return res.status(400).json({ error: "All required fields must be filled." });
+    }
+
+    const referral = await prisma.referral.create({
+      data: { name, email, referralCode },
+    });
+
+    await transporter.sendMail({
+      from: process.env.GMAIL_USER,
+      to: "trachit752@gmail.com",  // admin who will check the referral code belongs to which user for now.This can easily be automated.
+      subject: "New Referral Submission",
+      text: `Referral Details:\n\nName: ${name}\nEmail: ${email}\nReferral Code: ${referralCode}`,
+    });
+
+    res.status(201).json({ message: "Referral submitted successfully", referral });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error", details: error.message });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ prisma/migrations/20250226044437_init/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX `Referral_referralCode_key` ON `Referral`;
